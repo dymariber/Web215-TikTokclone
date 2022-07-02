9e15566f1369b444b4d7ae48a2ffc8e99a0ae40a
dymariber <101956056+dymariber@users.noreply.github.com>
2022-07-01 22:27:31 -0400

    Add files via upload

--- tiktok-backend/data.js
+++ tiktok-backend/data.js
@@ -1,1 +1,24 @@
+const data = [
+    {
+        url:
+        'https://vod-progressive.akamaized.net/exp=1656713929~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3764%2F18%2F468821581%2F2083983874.mp4~hmac=b3d64db63adaeaef1a83fb05d51f40cb729453b7b6876a5665d979486be5646d/vimeo-prod-skyfire-std-us/01/3764/18/468821581/2083983874.mp4?filename=pexels-sathyaprabha-rakkimuthu-5613843.mp4',
+        channel:'dymonberrien',
+        description:"Beautiful Jellyfish",
+        song:'Black Beauty',
+        likes:'3432',
+        messages:'334',
+        shares:'22'   
+    },
 
+    {
+        url: "https://vod-progressive.akamaized.net/exp=1656718271~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F1811%2F18%2F459059758%2F2028703892.mp4~hmac=c404b78b736049723cbe38a3883bbab16ad1633d81ecd223ebcd682b2b799098/vimeo-prod-skyfire-std-us/01/1811/18/459059758/2028703892.mp4?filename=pexels-rulo-davila-5380467.mp4",
+        channel:'ssamanthaa',
+        description:"Paper Boat",
+        song:'Move it',
+        likes:'5443',
+        messages:'332',
+        shares:'12'
+    },
+];
+
+export default data; 
